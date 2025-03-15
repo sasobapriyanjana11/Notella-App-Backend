a7b3ae1148fa7831196d70844dbf8656065420af
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2025-03-15 14:09:44 +0530

    implement delete-note

--- index.js
+++ index.js
@@ -217,6 +217,36 @@ app.get("/get-all-notes", authonticationToken, async (req, res) => {
     }
 });
 
+//delete note
+app.delete("/delete-note/:noteId", authonticationToken, async (req, res) => {
+    const noteId=req.params.noteId;
+    const {user}=req.user;
+    try{
+        const note=await Note.findOne({_id:noteId,userId:user._id
+
+        });
+        if(!note){
+            return res.status(404).json({
+                error:true,
+                message:"Note not found"
+            });
+        }
+
+        await Note.deleteOne({_id:noteId,userId:user._id});
+        return res.json({
+            error:false,
+            message:"Note deleted successfully!",
+            
+        });
+        
+    }catch(error){
+        return res.status(500).json({
+            error:true,
+            message:"Internal server error"
+        });
+    }
+});
+
 
 
 app.listen(8000);
