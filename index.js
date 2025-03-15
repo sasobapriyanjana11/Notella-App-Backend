736d1a69f98cfbad3b24a397ae14c0b9c8278230
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2025-03-15 13:49:23 +0530

    implement update note

--- index.js
+++ index.js
@@ -154,6 +154,47 @@ app.post("/add-note", authonticationToken, async (req, res) => {
     }
 });
 
+//edit-note
+app.put("/edit-note/:noteId", authonticationToken, async (req, res) => {
+    const noteId=req.params.noteId;
+    const{title,content,tags,isPinned}=req.body;
+    const {user}=req.user;
+
+    if(!title && !content && !tags){
+        return res.status(400).json({
+            error:true,
+            message:"no changes provided"
+        });
+    }
+    try{
+        const note=await Note.findOne({_id:noteId,userId:user._id});
+
+        if(!note){
+            return res.status(404).json({
+                error:true,
+                message:"Note not found"
+            });
+        }
+        if(title)note.title=title;
+        if(content)note.content=content;
+        if(tags)note.tags=tags;
+        if(isPinned)note.isPinned=isPinned;
+
+        await note.save();
+        return res.json({
+            error:false,
+            note,
+            message:"Note Upadted Successfully!"
+        });
+
+    }catch(error){
+        return res.status(500).json({
+            error:true,
+            message:"Internal server error",
+        });
+    }
+});
+
 
 
 app.listen(8000);
